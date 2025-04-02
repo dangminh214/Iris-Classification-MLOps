9e8bb1615f148543d6e80b3d0a4e0ee026ce851a
dangminh214 <dangminh214@gmail.com>
2025-04-02 22:36:35 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.12
+LABEL authors="dang-minh"
+
+WORKDIR /src
+
+COPY model.pkl /src/model.pkl 
+COPY server.py /src/server.py
+COPY requirements.txt /src/requirements.txt
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8888
+
+CMD ["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "8888"]
---
+++ server.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI 
+import pickle
+
+with open("model.pkl", "rb") as f: 
+    model = pickle.load(f)
+
+class_names = ["Iris-Setosa", "Iris-Versicolour", "Iris-Virginica"]
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+
+    # handle function
+    return {"message": "ML model deployment"}
+
+@app.post("/predict")
+def predict(data: dict): 
+
+    prediction = model.predict([data["features"]])[0]
+    predicted_class = class_names[prediction]
+
+    return {"message": predicted_class}
+
